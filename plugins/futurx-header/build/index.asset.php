2698a3933a5ca9dfa876441be8a3049cffc71367
Ramiro Zapata <ramaxzapata@gmail.com>
2024-07-18 17:49:14 +0200

    first configuration

---
+++ plugins/futurx-header/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c1a6ec1a4ce9f84644d0');
---
+++ plugins/futurx-header/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '81ff31aa8fe78d50b96c');
